758ad42371f8e42e9f4c61e8051a14fc43638fa6
LizaKonovalova <liza_konovalova_2000234@mail.ru>
2017-01-11 23:53:44 +0300

    номер 3
    лол

---
+++ 3.incredible_sort/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void incredible_sort(){
+
+int N, k;
+//ввод
+cin » N » k;
+int massiv[N];
+for(int i=0;i<N;i++){
+cin » massiv[i];
+}
+
+int temp; // вре'менная переменная
+
+// Сортировка массива
+for(int i=0;i<(N-1);i++){
+for(int j=(N-1);j>i;j--){
+if(massiv[j]>massiv[j-1]){
+temp=massiv[j];
+massiv[j]=massiv[j-1];
+massiv[j-1]=temp;
+}
+}
+}
+//конец сортировки
+//вывод
+for(int i=0;i<N;i++) {
+cout « massiv[i];
+}
+}
+
+int main(){
+incredible_sort();
+return 0;
+}
